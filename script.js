5260c9834680d487ac74d84cdb702818f2a6f519
Monushia Zimri <125980202+Monushia@users.noreply.github.com>
2023-08-21 14:21:33 +0200

    Home-page update

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Home-page buttons */
+const hire = document.getElementById("hire-me")
+const download = document.getElementById("downloadCV")
+
+hire.addEventListener('click', function () {
+    window.location.href = "https://github.com/Monushia"; 
+}); 
+
+download.addEventListener('click', function() {
+    window.location.href = "https://www.linkedin.com/feed/"; 
+}); 
+
+
+
+
+
+
+
+
+
+
+

1d91cec4f2aec990f9c4810d12dc27782a1c9c30
Monushia Zimri <125980202+Monushia@users.noreply.github.com>
2023-08-27 14:28:47 +0200

    Update HTML

---
+++ script.js
@@ -0,0 +1,40 @@
+
+//Home-page buttons
+
+    // Get reference to the buttons
+    const hireMeButton = document.getElementById('hire-me');
+    const downloadCVButton = document.getElementById('downloadCV');
+  
+    // Add event listeners
+    hireMeButton.addEventListener('click', () => {
+      alert('You clicked the "Hire Me" button!');
+      window.location.href = 'https://github.com/Monushia';
+    });
+  
+    downloadCVButton.addEventListener('click', () => {
+      alert('You clicked the "Download CV" button!');
+      window.location.href = 'https://www.linkedin.com/feed/';
+    });
+  
+
+            
+
+
+
+//About-page 
+
+
+
+
+//Skills-page
+
+
+
+
+//Projects-page
+
+
+
+
+//Contact-page 
+

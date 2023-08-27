009524a0cd85ee14511c81b648c65c02ff6331a6
Monushia Zimri <125980202+Monushia@users.noreply.github.com>
2023-08-27 20:49:22 +0200

    Add slideshow functionality with JavaScript.

--- script.js
+++ script.js
@@ -15,25 +15,38 @@
       alert('You clicked the "Download CV" button!');
       window.location.href = 'https://www.linkedin.com/feed/';
     });
-  
-
-            
 
 
 
 //About-page 
 
 
-
-
 //Skills-page
-
-
 
 
 //Projects-page
 
+const slideContainer = document.querySelector('.slide-show');
+const slides = slideContainer.querySelector('.slide');
 
+let currentSlideIndex = 0; 
+
+function showSlide (index) {
+  for (const slide of slides){
+    slide.style.display = none; 
+  }
+
+  slides[index].style.display = 'block'; 
+}
+
+function nextSlide () {
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+  showSlide(currentSlideIndex); 
+}
+
+showSlide(currentSlideIndex); 
+
+setInterval(nextSlide, 5000); 
 
 
 //Contact-page 

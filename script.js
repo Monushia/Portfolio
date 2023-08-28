47a67d17113a687c0795f42a54e61e5876d15668
Monushia Zimri <125980202+Monushia@users.noreply.github.com>
2023-08-28 10:38:31 +0200

    Changed the id button name in html to match the id  button name in javascript for the code to be executed.

--- script.js
+++ script.js
@@ -1,72 +1,70 @@
+  //Home-page buttons 
 
-//Home-page buttons
-
-    // Get reference to the buttons
-    const hireMeButton = document.getElementById('hire-me');
-    const downloadCVButton = document.getElementById('downloadCV');
+        // Get reference to the buttons
+        const hireMeButton = document.getElementById("hire-me");
+        const downloadCVButton = document.getElementById("downloadCV");
   
-    // Add event listeners
-    hireMeButton.addEventListener('click', () => {
-      alert('You clicked the "Hire Me" button!');
-      window.location.href = 'https://github.com/Monushia';
-    });
+        // Add event listeners
+        hireMeButton.addEventListener('click', () => {
+            alert('You clicked the "Hire Me" button!');
+            window.location.href = 'https://github.com/Monushia';
+        }); 
   
-    downloadCVButton.addEventListener('click', () => {
-      alert('You clicked the "Download CV" button!');
-      window.location.href = 'https://www.linkedin.com/feed/';
-    });
+        downloadCVButton.addEventListener('click', () => {
+            alert('You clicked the "Download CV" button!');
+            window.location.href = 'https://www.linkedin.com/feed/';
+        });
 
 
-
-//About-page 
-
-
-//Skills-page
+        //About-page 
 
 
-//Projects-page
-
-const slideContainer = document.querySelector('.slide-show');
-const slides = slideContainer.querySelector('.slide');
-
-let currentSlideIndex = 0; 
-
-function showSlide (index) {
-  for (const slide of slides){
-    slide.style.display = none; 
-  }
-
-  slides[index].style.display = 'block'; 
-}
-
-function nextSlide () {
-  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-  showSlide(currentSlideIndex); 
-}
-
-showSlide(currentSlideIndex); 
-
-setInterval(nextSlide, 5000);  
+        //Skills-page
 
 
-//Contact-page 
+        //Projects-page
 
-const sendButton = document.getElementById("send-btn");
-const contactForm = document.getElementById("contact-form"); 
+        const slideContainer = document.querySelector(".slide-show");
+        const slides = slideContainer.querySelectorAll(".slide");
 
-sendButton.addEventListener('click', function () {
-  const name = document.getElementById('name').value;
-  const subject = document.getElementById('subject').value;
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
+        let currentSlideIndex = 0; 
 
-  if (name && subject && email & message) {
-    alert('Form submitted successfully!'); 
-    contactForm.reset(); 
-  } else {
-    alert('Please fill in all required fields.'); 
-  }
-}); 
+        function showSlide (index) {
+            for (const slide of slides){
+            slide.style.display = 'none'; 
+        }
+
+        slides[index].style.display = 'block'; 
+        }
+
+        function nextSlide () {
+            currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+            showSlide(currentSlideIndex); 
+        }
+
+        showSlide(currentSlideIndex); 
+
+        setInterval(nextSlide, 5000);  
+
+
+        //Contact-page 
+
+        const sendButton = document.getElementById('send.btn');
+        const contactForm = document.getElementById("contact-form"); 
+
+        sendButton.addEventListener('click', () => {
+            const name = document.getElementById('name').value;
+            const subject = document.getElementById('subject').value;
+            const email = document.getElementById('email').value;
+            const message = document.getElementById('message').value;
+
+            if (name && subject && email & message) {
+            alert('Form submitted successfully!'); 
+            contactForm.reset(); 
+            } else {
+            alert('Please fill in all required fields.'); 
+            }
+        });  
 
 
 
